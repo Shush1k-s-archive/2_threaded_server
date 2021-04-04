77a2271b50a68e37eb14283aeb9f227a27624e6d
Shush1k <Shush1k.b@gmail.com>
2021-04-04 10:16:39 +0300

    Threaded server
    - несколько подключений к серверу

--- client.py
+++ client.py
@@ -1,16 +1,144 @@
 import socket
+import logging
+from threading import Thread
+import sys
+import pickle
+from validation import ip_validation, port_validation
+from getpass import getpass
 from time import sleep
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+IP_DEFAULT = "127.0.0.1"
+PORT_DEFAULT = 9090
+logging.basicConfig(filename='log/client.log',
+                    format="%(asctime)s [%(levelname)s] %(funcName)s: %(message)s", level=logging.INFO)
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+class Client:
+    """
+    Клиент
+    """
 
-data = sock.recv(1024)
+    def __init__(self, server_ip, port, status = None):
+        """
+        Args:
+            server_ip (str): localhost/ip-адресс сервера
+            port (int): порт сервера
+            status (str): текущее состояние программы. Defaults to None.
+        """
+        self.server_ip = server_ip
+        self.port = port
+        self.status = status
+        self.server_connection()
+        self.polling()
 
-sock.close()
+    def server_connection(self):
+        """
+        Соединение пользователя с сервером
+        """
+        sock = socket.socket()
+        sock.setblocking(1)
+        try:
+            sock.connect((self.server_ip, self.port))
+        except ConnectionRefusedError:
+            print(f"Не удалось присоединиться к серверу {self.server_ip, self.port}")
+            sys.exit(0)
+        self.sock = sock
+        logging.info(
+            f"Установлено соединение {self.sock.getsockname()} с сервером ('{self.server_ip}', {self.port})")
 
-print(data.decode())
+    def polling(self):
+        """
+        Проверяем какой статус приложения
+        """
+        Thread(target=self.recv).start()
+        print("Используйте 'exit', чтобы разорвать соединение")
+        while self.status != 'finish':
+            if self.status:
+                if self.status == "auth":
+                    self.auth()
+                    logging.info(f"Пользователь {self.sock.getsockname()} зарегистрировался")
+                elif self.status == "passwd":
+                    self.sendPasswd()
+                elif self.status == "success":
+                    self.success()
+                else:
+                    msg = input(f"{self.username}> ")
+                    if msg != "":
+                        if msg == "exit":
+                            self.status = "finish"
+                            logging.info(f"Разрыв соединения {self.sock.getsockname()} с сервером")
+                            break
+                        # Отправляем сообщение и имя клиента
+                        sendM = pickle.dumps(["message", msg, self.username])
+                        self.sock.send(sendM)
+                        logging.info(f"Отправка данных от {self.sock.getsockname()} на сервер: {msg}")
+        self.sock.close()
+
+    def sendPasswd(self):
+        """
+        Отправка пароля на сервер
+        """
+        passwd = getpass(self.data)
+        self.sock.send(pickle.dumps(["passwd", passwd]))
+        # если убрать sleep ничего работать не будет!!!
+        sleep(1.5)
+
+    def auth(self):
+        """
+        Отправка имени на сервер
+        """
+        print("Введите имя:")
+        self.username = input()
+        self.sock.send(pickle.dumps(["auth", self.username]))
+        # если убрать sleep ничего работать не будет!!!
+        sleep(1.5)
+
+    def success(self):
+        """
+        Вывод приветственного сообщения
+        """
+        print(self.data)
+        self.status = "ready"
+        self.username = self.data.split(" ")[1]
+        logging.info(f"Клиент {self.sock.getsockname()} прошел авторизацию")
+
+    def recv(self):
+        """
+        Функция получения данных
+        Работает в отдельном потоке
+        """
+        while True:
+            try:
+                self.data = self.sock.recv(1024)
+                if not self.data:
+                    sys.exit(0)
+                status = pickle.loads(self.data)[0]
+                self.status = status
+                if self.status == "message":
+                    print(f"\n{pickle.loads(self.data)[2]} -->", pickle.loads(self.data)[1])
+                    # можно проверить с помощью двух присоединений к серверу
+                    logging.info(f"Клиент {self.sock.getsockname()} принял данные от сервера: {pickle.loads(self.data)[1]}")
+                else:
+                    self.data = pickle.loads(self.data)[1]
+            except OSError:
+                break
+
+
+def main():
+    """
+    Ввод порта и ip сервера, валидация данных
+    """
+    user_port = input("Введите порт (enter для значения по умолчанию):")
+    if not port_validation(user_port):
+        user_port = PORT_DEFAULT
+        print(f"Установили порт {user_port} по умолчанию")
+
+    user_ip = input("Введите ip сервера (enter для значения по умолчанию):")
+    if not ip_validation(user_ip):
+        user_ip = IP_DEFAULT
+        print(f"Установили ip-адресс {user_ip} по умолчанию")
+
+    Client(user_ip, int(user_port))
+
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,20 +1,214 @@
+import hashlib
+import json
+import logging
+import pickle
 import socket
+from threading import Thread
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+from validation import is_free_port, port_validation
 
-msg = ''
+PORT_DEFAULT = 9090
+logging.basicConfig(format="%(asctime)s [%(levelname)s] %(funcName)s: %(message)s",
+                    handlers=[logging.FileHandler("log/server.log"), logging.StreamHandler()], level=logging.INFO)
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
 
-print(msg)
+class Server():
+    """
+    Сервер
+    """
 
-conn.close()
+    def __init__(self, port, clients=[], status=None):
+        """
+        Args:
+            port (int): порт сервера
+            clients (list): список подключенных клиентов. Defaults to [].
+            status (str): текущее состояние программы. Defaults to None.
+        """
+        # users - файл с данными
+        self.users = "users.json"
+        self.clients = clients
+        self.server_port = port
+        self.all_Users = []
+        self.status = status
+        self.server_boot()
+
+    def readJSON(self):
+        """
+        Читаем файл
+        
+        Returns:
+            list: список пользователей
+        """
+        with open(self.users, 'r') as f:
+            users = json.load(f)
+        return users
+
+    def writeJSON(self):
+        """
+        Запись всех пользователей в файл
+        """
+        with open(self.users, 'w') as f:
+            json.dump(self.all_Users, f, indent=4)
+
+    def server_boot(self):
+        """
+        Запуск сервера
+        """
+        sock = socket.socket()
+        sock.bind(('', self.server_port))
+        sock.listen(5)
+        self.sock = sock
+        logging.info(f"Сервер стартанул, слушаем порт {self.server_port}")
+        while True:
+            conn, addr = self.sock.accept()
+            Thread(target=self.listenToClient, args=(conn, addr)).start()
+            logging.info(f"Подключение клиента {addr}")
+            self.clients.append(conn)
+
+    def broadcast(self, msg, conn, address, username):
+        """
+        Отправка данных клиентам\n
+        Отправляем сообщение и имя пользователя с номером соединения
+
+        Args:
+            msg (str): сообщение
+            conn (socket): сокет с данными клиента
+            address (tuple): кортеж ip-адреса и номера соединения
+            username (str): имя клиента
+        """
+        username += "_" + str(address[1])
+        for sock in self.clients:
+            if sock != conn:
+                data = pickle.dumps(["message", msg, username])
+                sock.send(data)
+                logging.info(f"Отправка данных клиенту {sock.getsockname()}: {msg}")
+
+    @staticmethod
+    def check_password(passwd, userkey):
+        """
+        Проверяем пароль из файла и введенный пользователем
+
+        Args:
+            passwd (str): пароль введенный пользователем
+            userkey (str): хранимый пароль пользователя
+
+        Returns:
+            boolean: True/False
+        """
+        key = hashlib.md5(passwd.encode() + b'salt').hexdigest()
+        return key == userkey
+
+    @staticmethod
+    def generate_hash(passwd):
+        """
+        Генерация пароля\n
+        Args:
+            passwd (str): пароль
+
+        Returns:
+            str: хэш пароль
+        """
+        key = hashlib.md5(passwd.encode() + b'salt').hexdigest()
+        return key
+
+    def listenToClient(self, conn, address):
+        """
+        Слушаем клиента, если данные есть отправляем их клиентам.\n
+        Иначе закрываем соединение клиента.
+
+        Args:
+            conn (socket): сокет с данными клиента
+            address (tuple): кортеж ip-адреса и номера соединения
+        """
+        self.authorization(address, conn)
+        while True:
+            try:
+                data = conn.recv(1024)
+            except ConnectionResetError:
+                conn.close()
+                self.clients.remove(conn)
+                logging.info(f"Отключение клиента {address}")
+                break
+
+            if data:
+                status, data, username = pickle.loads(data)
+                logging.info(f"Прием данных от клиента '{username}_{address[1]}': {data}")
+                if status == "message":
+                    self.broadcast(data, conn, address, username)
+
+            else:
+                # Закрываем соединение
+                conn.close()
+                self.clients.remove(conn)
+                logging.info(f"Отключение клиента {address}")
+                break
+
+    def authorization(self, addr, conn):
+        """
+        Авторизация, считывание информации из файла
+
+        Args:
+            addr (tuple): кортеж ip-адреса и номера соединения
+            conn (socket): сокет с данными клиента
+        """
+        # Проверка есть ли в файле данные
+        try:
+            self.all_Users = self.readJSON()
+        except json.decoder.JSONDecodeError:
+            self.registration(addr, conn)
+
+        user_flag = False
+        for user in self.all_Users:
+            if addr[0] in user:
+                for k, v in user.items():
+                    if k == addr[0]:
+                        name = v['name']
+                        password = v['password']
+                        conn.send(pickle.dumps(["passwd", "Введите свой пароль: "]))
+                        passwd = pickle.loads(conn.recv(1024))[1]
+                        conn.send(pickle.dumps(["success", f"Здравствуйте, {name}"])) if self.check_password(
+                            passwd, password) else self.authorization(addr, conn)
+                        user_flag = True
+        # Если пользователь не найден в файле
+        if not user_flag:
+            self.registration(addr, conn)
+
+    def registration(self, addr, conn):
+        """
+        Регистрация, запись данных в файл, обновление списка клиентов
+
+        Args:
+            addr (tuple): кортеж ip-адреса и номера соединения
+            conn (socket): сокет с данными клиента
+        """
+        conn.send(pickle.dumps(
+            ["auth", ""]))
+        name = pickle.loads(conn.recv(1024))[1]
+        conn.send(pickle.dumps(["passwd", "Введите свой пароль: "]))
+        passwd = self.generate_hash(pickle.loads(conn.recv(1024))[1])
+        conn.send(pickle.dumps(["success", f"Приветствую, {name}"]))
+        self.all_Users.append({addr[0]: {'name': name, 'password': passwd}})
+        # Запись в файл при регистрации пользователя
+        self.writeJSON()
+        self.all_Users = self.readJSON()
+
+
+def main():
+    # Подбор порта
+    server_port = PORT_DEFAULT
+    if not port_validation(PORT_DEFAULT, True):
+        if not is_free_port(PORT_DEFAULT):
+            logging.info(f"Порт по умолчанию {PORT_DEFAULT} занят")
+            free_port = False
+            # перебор порта
+            while not free_port:
+                server_port += 1
+                free_port = is_free_port(server_port)
+    try:
+        Server(server_port)
+    except KeyboardInterrupt:
+        logging.info(f"Остановка сервера")
+
+
+if __name__ == "__main__":
+    main()
